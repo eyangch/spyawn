fc841e9f46cb641ac17ce1cba50c8951f1dd6338
Eric Yang <eyangch@gmail.com>
2023-07-13 22:33:55 +0000

    Add deletion functionality

--- spyawn.py
+++ spyawn.py
@@ -3,10 +3,13 @@ import docker
 import json
 import time
 import random
+import os
 
 client = docker.from_env()
 
 config = json.load(open("config.json"))
+if not os.path.isdir("data"):
+    os.mkdir("data")
 
 app = Flask(__name__)
 
@@ -18,12 +21,26 @@ def make_container(uid, chal):
     while True:
         try:
             port = random.randint(port_range[0], port_range[1])
-            container = client.containers.run(chal, ports={str(config[chal]["internal_port"]) + "/tcp": port}, **(config[chal]["container"]))
+            container = client.containers.run(chal, ports={str(config[chal]["internal_port"]) + "/tcp": port}, name=chal+str(uid), **(config[chal]["container"]))
             break
         except Exception as e:
             print(e)
+    print("CREATED: " + str(uid) + " " + chal)
     with open("data/" + str(uid) + "." + chal, "w") as f:
         json.dump({"time": round(time.time() + config[chal]["container"]["healthcheck"]["interval"]//1000000000), "port": port}, f)
+
+
+def del_container(uid, chal):
+    container = None
+    print("KILLED: " + str(uid) + " " + chal)
+    try:
+        container = client.containers.get(chal + str(uid))
+    except Exception as e:
+        print(e)
+        return
+    container.kill()
+    with open("data/" + str(uid) + "." + chal, "w") as f:
+        json.dump({"time": 0, "port": 0}, f)
 
 @app.route("/get/<int:uid>/<chal>")
 def get(uid, chal):
@@ -33,6 +50,16 @@ def get(uid, chal):
     except:
         pass
     return res
+
+@app.route("/remove/<int:uid>/<chal>")
+def remove(uid, chal):
+    if chal not in config:
+        return {"status": "error", "message": "invalid chal"}
+    chal_dat = get(uid, chal)
+    if chal_dat["time"] < round(time.time()):
+        return {"status": "error", "message": "chal already deleted"}
+    del_container(uid, chal)
+    return {"status": "yay"}
 
 @app.route("/create/<int:uid>/<chal>")
 def create(uid, chal):
