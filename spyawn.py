c5a8a84290d491af560dca8ff5080b3d4f564c49
eyangch <eyangch@gmail.com>
2023-07-20 22:00:58 -0500

    add rate limit and id verification

--- spyawn.py
+++ spyawn.py
@@ -4,12 +4,15 @@ import json
 import time
 import random
 import os
+import requests
 
 client = docker.from_env()
 
 config = json.load(open("config.json"))
 if not os.path.isdir("data"):
     os.mkdir("data")
+if not os.path.isdir("rates"):
+    os.mkdir("rates")
 
 app = Flask(__name__)
 
@@ -29,7 +32,6 @@ def make_container(uid, chal):
     with open("data/" + str(uid) + "." + chal, "w") as f:
         json.dump({"time": round(time.time() + config[chal]["container"]["healthcheck"]["interval"]//1000000000), "port": port}, f)
 
-
 def del_container(uid, chal):
     container = None
     print("KILLED: " + str(uid) + " " + chal)
@@ -42,8 +44,28 @@ def del_container(uid, chal):
     with open("data/" + str(uid) + "." + chal, "w") as f:
         json.dump({"time": 0, "port": 0}, f)
 
+def verify_id(uid):
+    r = requests.get("http://5.161.201.219:8000/api/checkTeamId/" + str(uid))
+    return r.json()["teamIdExists"]
+
+def ratelimit(ip):
+    if not os.path.isfile("rates/" + ip):
+        with open("rates/" + ip, "w") as f:
+            f.write("0")
+    with open("rates/" + ip) as f:
+        prev_t = int(f.read())
+        if prev_t == round(time.time()):
+            return False
+    with open("rates/" + ip, "w") as f:
+        f.write(str(round(time.time())))
+    return True
+
 @app.route("/get/<int:uid>/<chal>")
 def get(uid, chal):
+    if not ratelimit(request.remote_addr):
+        return {"status": "error", "message": "rate limit exceeded"}
+    if not verify_id(uid):
+        return {"status": "error", "message": "invalid team"}
     res = {"time": 0, "port": 0}
     try:
         res = json.load(open("data/" + str(uid) + "." + chal))
@@ -53,6 +75,10 @@ def get(uid, chal):
 
 @app.route("/remove/<int:uid>/<chal>")
 def remove(uid, chal):
+    if not ratelimit(request.remote_addr):
+        return {"status": "error", "message": "rate limit exceeded"}
+    if not verify_id(uid):
+        return {"status": "error", "message": "invalid team"}
     if chal not in config:
         return {"status": "error", "message": "invalid chal"}
     chal_dat = get(uid, chal)
@@ -63,6 +89,10 @@ def remove(uid, chal):
 
 @app.route("/create/<int:uid>/<chal>")
 def create(uid, chal):
+    if not ratelimit(request.remote_addr):
+        return {"status": "error", "message": "rate limit exceeded"}
+    if not verify_id(uid):
+        return {"status": "error", "message": "invalid team"}
     if chal not in config:
         return {"status": "error", "message": "invalid chal"}
     chal_dat = get(uid, chal)
